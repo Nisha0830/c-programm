821c1f2f2f935e899e4f03f7f22ede6543a10f7e
Nisha0830 <47448822+Nisha0830@users.noreply.github.com>
2019-02-08 18:22:11 +0530

    Update and rename README.md to strrep.c

---
+++ strrep.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int k,i;
+    char s[100];
+    scanf("%s ",&s);
+    scanf("%d",&k);
+    for(i=0;i<k;i++)
+    {
+    printf("%s\n",s);   
+    }
+    return 0;
+}
